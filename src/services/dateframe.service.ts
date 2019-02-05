b4b4ee6ed2ee7177d01727dc5c6d0d2a011a28f5
Kudsy <Firas.Kudsy@td.com>
2019-02-05 13:29:25 -0500

    fix

--- src/services/dateframe.service.ts
+++ src/services/dateframe.service.ts
@@ -89,20 +89,66 @@ export class DateframeService {
     });
   }
 
+  get_first_date(date: Date) {
+    let temp_date = new Date(date);
+
+    return new Promise((resolve, reject) => {
+      let d = 0;
+      while ((this.is_BOD(temp_date) || !this.in_slot(temp_date)) && d <= 14) {
+        d++;
+        temp_date = this.add_days(temp_date, 1);
+      }
+      if (d > 14) {
+        reject('no date available within 14 days, either all blocked out days or no active slot');
+      }
+      resolve(temp_date); // should return the selected slot time
+    });
+  }
+
   get_date_time(date: Date) {
     const temp_date = new Date(date);
 
-    return Object.assign({}, {
-      day_name: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][temp_date.getDay()],
-      year: temp_date.getFullYear(),
-      month: temp_date.getMonth() + 1,
-      day: temp_date.getDate(),
-      hour: temp_date.getHours(),
-      minute: temp_date.getMinutes(),
-      am_pm: this.get_date_format(temp_date, 'a'),
-      date: this.get_date_format(temp_date, 'yyyy-MM-dd'),
-      time: this.get_date_format(temp_date, 'hh:mm a')
-    });
+    return Object.assign(
+      {},
+      {
+        day_name: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][temp_date.getDay()],
+        year: temp_date.getFullYear(),
+        month: temp_date.getMonth() + 1,
+        day: temp_date.getDate(),
+        hour: temp_date.getHours(),
+        minute: temp_date.getMinutes(),
+        am_pm: this.get_date_format(temp_date, 'a'),
+        date: this.get_date_format(temp_date, 'yyyy-MM-dd'),
+        time: this.get_date_format(temp_date, 'hh:mm a'),
+        _init: temp_date
+      }
+    );
+  }
 
+  asap_run() {
+    const _today = new Date();
+    this.get_offset_date(_today)
+      .then((target: Date) => {
+        this.get_first_date(target)
+          .then((result: Date) => {
+            if (result) {
+              this.get_first_active_slot(result)
+                .then(slot => {
+                  console.log(' asap result ', result, slot);
+                })
+                .catch(error => {
+                  console.log('error get_first_active_slot ', error);
+                });
+            } else {
+              console.log('invalid get_first_date result', result);
+            }
+          })
+          .catch(error => {
+            console.log('error get_first_date ', error);
+          });
+      })
+      .catch(error => {
+        console.log('error get_offset_date', error);
+      });
   }
 }
